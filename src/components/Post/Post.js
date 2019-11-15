b3d4f51a55dcbfc9c87b0870c1844b73bfb2304f
dpouls <dallinpoulson@gmail.com>
2019-11-15 16:31:24 -0700

    Finished, not Black Diamond

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -54,6 +54,12 @@ export default class Post extends Component {
       // const editing = this.state.editing
       // const showMasterMenu = this.state.showMasterMenu
     const { editing, showMasterMenu } = this.state;
+    const date = this.props.date;
+    const text = this.props.text;
+    const id= this.props.id
+    const updatePostFn = this.props.updatePostFn
+    const deletePostFn = this.props.deletePostFn
+    const createPostFn = this.props.createPostFn
 
     return (
       // Main body of post
@@ -66,7 +72,7 @@ export default class Post extends Component {
           {/* Drop-down menu. Remember that the "showMasterMenu" variable has been destructured off of this.state */}
           <div className="Post__master-menu" style={ { display: showMasterMenu ? 'flex' : 'none' } }>
             <span onClick={ this.showEdit }>Edit</span>
-            <span>Delete</span>
+            <span onClick={ () => deletePostFn(id)}>Delete</span>
           </div>
         </div>
 
@@ -79,7 +85,7 @@ export default class Post extends Component {
           <span className="Post__name">DevMountain</span>
           <span className="Post__handle">@DevMountain</span>
 
-          <span className="Post__date">- POST DATE GOES HERE</span>
+          <span className="Post__date">{ date }</span>
         </div>
 
         {/* This is where the text goes. Notice the turnary statement. The turnary statement decides to display either the text OR the editor view
@@ -95,10 +101,12 @@ export default class Post extends Component {
             // This has been pulled off of this.state via destructuring
             editing
             ?
-              <Edit text=""
-                    hideEdit={ this.hideEdit } />
+              <Edit text={ text }
+                    hideEdit={ this.hideEdit } 
+                    updatePostFn={updatePostFn}
+                    id={ id }/>
             :
-              <span className="Post__text">POST TEXT GOES HERE</span>
+              <span className="Post__text">{text}</span>
           }
         </div>
 
